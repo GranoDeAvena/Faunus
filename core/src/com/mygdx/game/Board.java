e50c635570a615ff27e70cc6018a15975d4f5c06
dm0123 <dm0123@gmail.com>
2015-01-30 22:51:49 +0300

    поправил падение

--- core/src/com/mygdx/game/Board.java
+++ core/src/com/mygdx/game/Board.java
@@ -1,15 +1,20 @@
 package com.mygdx.game;
 
+import java.util.Vector;
+
+import java.util.List;
+
 /**
  * Created by Дмитрий on 30.01.2015.
  */
 public class Board {
-    public Cage c[];
-
+//    public Cage c[] = new Cage[625];
+    public Vector<Cage> c = new Vector<Cage>();
     public Board () {
-        c[625] = new Cage();
-        c[1].init(1);
-        //for (int i = 1; i < 625; i++)
-        //    c[i].init (i);
+//        c[625] = new Cage();
+//        c[1].init(1);
+        for (int i = 1; i < 625; i++)
+            c.add(new Cage());
+        c.get(1).init(1);
     }
 }
